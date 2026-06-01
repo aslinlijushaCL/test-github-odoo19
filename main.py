02921fa1be2ca1380a35a094e1af33bf23046930
Aslin <aslin@cloudliongroup.com>
2026-06-01 16:06:01 +0530

    test

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 # Hi
 def greet_user():
     name = input("What's your name? ")
-    print(f"Hello, (name)! Welcome to the program.")
+    print(f"Hello, {name}! Welcome to the program.")
 
 # Call the function
 greet_user()
